d7fceb64058d507c1213519d93cc4274e5ec14be
jioo <rlawl3383@gmail.com>
2024-12-23 23:58:24 +0900

    mswRunner 프로덕션 + dev 까지 사용범위 수정

--- src/app/utils/MswRunner.tsx
+++ src/app/utils/MswRunner.tsx
@@ -4,28 +4,21 @@ import { useEffect } from "react";
 import { useMswReady } from "../store/common";
 
 export const MSWComponent = () => {
-  let workerStarted = false;
   useEffect(() => {
-    if (
-      typeof window !== "undefined" &&
-      process.env.NODE_ENV === "development"
-    ) {
+    if (typeof window !== "undefined") {
       console.log("Initializing MSW...");
       import("../mocks/browser")
-        .then((module) => {
-          const { worker } = module;
-          if (!workerStarted) {
-            worker
-              .start({
-                onUnhandledRequest: "bypass",
-              })
-              .then(() => {
-                useMswReady.setState({ loading: true });
-              });
-            workerStarted = true; // 상태를 업데이트하여 중복 호출 방지
-          }
+        .then(({ worker }) => {
+          worker
+            .start({
+              onUnhandledRequest: "bypass", // 정의되지 않은 요청은 실제 네트워크로 전달
+            })
+            .then(() => {
+              console.log("MSW is running.");
+              useMswReady.setState({ loading: true }); // 상태 업데이트
+            });
         })
-        .catch(() => console.log("msw 실행 안됨"));
+        .catch(() => console.error("MSW initialization failed."));
     }
   }, []);
 
